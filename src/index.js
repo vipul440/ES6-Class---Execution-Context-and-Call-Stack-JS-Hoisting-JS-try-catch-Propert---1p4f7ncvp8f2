c1eea91e9f45a01dfd89fee43be8100d72cac1e7
vipul440 <82093511+vipul440@users.noreply.github.com>
2023-04-22 13:06:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-
-
+class API{
+  url;
+  method='GET';
+  #secure;
+  
+  constructor(url){
+    this.url=url;
+    this.#secure=url.startsWith('https');
+}
+  isSecure(){
+    return this.#secure;
+  }
+  updateURL(){
+    this.url=url;
+    this.#secure=url.startsWith('https');
+  }
+}
 module.exports = { API }
